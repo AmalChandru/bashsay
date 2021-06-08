1081b8b9f76c63b305a9e38290c8d51e98c529f6
AswathiGR <44152778+AswathiGR@users.noreply.github.com>
2021-06-09 01:29:54 +0530

    Create getanim.sh

---
+++ AnimFace/getanim.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+sh ./intro.sh
+
+sleep 2
+cat frame1
+sleep 1
+clear
+cat frame2
+sleep 1
+clear
+cat frame1
+sleep 1
+clear
+cat frame2
+sleep 1
+clear
+cat frame1
+sleep 1
+clear
+cat frame2
+sleep 1
+clear
+cat frame1
+sleep 1
+clear
+cat frame2
+sleep 1
+clear
+cat frame1
+sleep 1
+clear
+cat frame2
+sleep 1
+clear
+cat frame1
+sleep 1
+clear
+cat frame2
+sleep 1
+clear
+cat frame1
+sleep 1
+clear
+cat frame2
+sleep 1
+clear
+cat frame1
+sleep 1
+clear
+cat frame2
+sleep 1
+clear
+cat frame1
+sleep 1
+clear
+cat frame2
+sleep 1
+clear
+cat frame1
+sleep 1
+clear
+cat frame2
+sleep 1
+clear
+cat frame1
+sleep 1
+clear
+cat frame2
+sleep 1
+clear
+cat frame1
+sleep 1
+clear
+cat frame2
+sleep 1
+clear
+cat frame1
+sleep 1
+clear
+cat frame2
+sleep 1
+clear
+cat frame1
+sleep 1
+clear
+cat frame2
+sleep 1
+clear
