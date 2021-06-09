b80ed9c79dcb7077725e5b946332d6237449e47d
AswathiGR <44152778+AswathiGR@users.noreply.github.com>
2021-06-09 11:01:44 +0530

    Update getanim.sh

--- AnimFace/getanim.sh
+++ AnimFace/getanim.sh
@@ -3,87 +3,16 @@
 sh ./intro.sh
 
 sleep 2
-cat frame1
-sleep 1
-clear
-cat frame2
-sleep 1
-clear
-cat frame1
-sleep 1
-clear
-cat frame2
-sleep 1
-clear
-cat frame1
-sleep 1
-clear
-cat frame2
-sleep 1
-clear
-cat frame1
-sleep 1
-clear
-cat frame2
-sleep 1
-clear
-cat frame1
-sleep 1
-clear
-cat frame2
-sleep 1
-clear
-cat frame1
-sleep 1
-clear
-cat frame2
-sleep 1
-clear
-cat frame1
-sleep 1
-clear
-cat frame2
-sleep 1
-clear
-cat frame1
-sleep 1
-clear
-cat frame2
-sleep 1
-clear
-cat frame1
-sleep 1
-clear
-cat frame2
-sleep 1
-clear
-cat frame1
-sleep 1
-clear
-cat frame2
-sleep 1
-clear
-cat frame1
-sleep 1
-clear
-cat frame2
-sleep 1
-clear
-cat frame1
-sleep 1
-clear
-cat frame2
-sleep 1
-clear
-cat frame1
-sleep 1
-clear
-cat frame2
-sleep 1
-clear
-cat frame1
-sleep 1
-clear
-cat frame2
-sleep 1
-clear
+
+
+x=0
+while [ $x -le 5 ]
+do
+   cat frame1
+    sleep 1
+    clear
+    cat frame2
+    sleep 1
+    clear
+  
+done
